93c4e566659f4871da820cfa8233fcf4fff439c2
Khaled-Murrar <khalouchdev@gmail.com>
2022-08-07 23:01:29 +0300

    White Lamp

--- src/main/java/net/khaled/extralamps/block/ModBlocks.java
+++ src/main/java/net/khaled/extralamps/block/ModBlocks.java
@@ -29,6 +29,7 @@ public class ModBlocks {
     public static final Block BROWN_LAMP = registerBlock("brown_lamp", new LightBlueLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(BrownLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
     public static final Block GRAY_LAMP = registerBlock("gray_lamp", new GrayLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(GrayLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
     public static final Block LIGHT_GRAY_LAMP = registerBlock("light_gray_lamp", new LightGrayLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(LightGrayLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
+    public static final Block WHITE_LAMP = registerBlock("white_block", new WhiteLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(WhiteLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
