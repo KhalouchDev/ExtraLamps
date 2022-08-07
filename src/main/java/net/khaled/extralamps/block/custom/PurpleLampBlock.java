f436f2877a19564e3ffc21b50d42dfa39e06e74d
Khaled-Murrar <khalouchdev@gmail.com>
2022-08-07 16:51:52 +0300

    Purple Lamp

---
+++ src/main/java/net/khaled/extralamps/block/custom/PurpleLampBlock.java
@@ -0,0 +1,32 @@
+package net.khaled.extralamps.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class PurpleLampBlock extends Block {
+    public static final BooleanProperty LIT = BooleanProperty.of("lit");
+
+    public PurpleLampBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState((BlockState)this.getDefaultState().with(LIT, false));
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if(!world.isClient){
+            boolean bl = state.get(LIT);
+            if(bl != world.isReceivingRedstonePower(pos)) {
+                world.setBlockState(pos, state.cycle(LIT));
+            }
+        }
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(LIT);
+    }
+}
