7a9851b67bc737a76302e870a0363e6a72f7648e
Khaled-Murrar <khalouchdev@gmail.com>
2022-08-07 14:44:54 +0300

    Magenta Lamp

--- src/main/java/net/khaled/extralamps/block/ModBlocks.java
+++ src/main/java/net/khaled/extralamps/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.khaled.extralamps.ExtraLamps;
 import net.khaled.extralamps.block.custom.BlueLampBlock;
 import net.khaled.extralamps.block.custom.GreenLampBlock;
+import net.khaled.extralamps.block.custom.MagentaLampBlock;
 import net.khaled.extralamps.block.custom.RedLampBlock;
 import net.khaled.extralamps.item.ModItemGroup;
 import net.minecraft.block.Block;
@@ -21,6 +22,7 @@ public class ModBlocks {
     public static final Block BLUE_LAMP = registerBlock("blue_lamp", new BlueLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(BlueLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
     public static final Block RED_LAMP = registerBlock("red_lamp", new RedLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(RedLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
     public static final Block GREEN_LAMP = registerBlock("green_lamp", new GreenLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(GreenLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
+    public static final Block MAGENTA_LAMP = registerBlock("magenta_lamp", new MagentaLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).strength(0.3f).luminance(state -> state.get(MagentaLampBlock.LIT) ? 15 : 0).sounds(BlockSoundGroup.GLASS)), ModItemGroup.LAMPS);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
